714b4f4fd9e086068fee63a69b338263c19bbf93
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-11 17:22:10 -0300

    Trabalhando com props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { React } from 'react'
+import Botao from './components/botao/botao'
 
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <Botao buttonmsg="teste123"/>
     </>
   )
 }
---
+++ src/components/botao/botao.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react'
+
+
+export default function Botao(props) {
+
+  return (
+    <>
+      <button>{props.buttonmsg}</button>
+    </>
+  )
+}
+
